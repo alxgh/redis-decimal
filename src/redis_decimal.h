d9541b17de94f378fb081c724c71edb9da8e23e3
Hamed Ghoochanian <H.Ghoochanian@webmail.ompfinex.io>
2023-06-26 18:28:15 +0330

    Added support for decimal operations on hashmap values

--- src/redis_decimal.h
+++ src/redis_decimal.h
@@ -7,38 +7,44 @@
 
 using boost::multiprecision::cpp_dec_float_50;
 
-template<int Prec>
-class RedisDecimal : public cpp_dec_float_50 {
- public:
+template <int Prec>
+class RedisDecimal : public cpp_dec_float_50
+{
+public:
   explicit RedisDecimal(const char *value) : cpp_dec_float_50(std::string(value)) {}
 
   explicit RedisDecimal(const RedisModuleString *value)
-          : cpp_dec_float_50(std::string(RedisModule_StringPtrLen(value, NULL))) {}
+      : cpp_dec_float_50(std::string(RedisModule_StringPtrLen(value, NULL))) {}
 
-  RedisModuleString *toRedisString(RedisModuleCtx *ctx) const {
+  RedisModuleString *toRedisString(RedisModuleCtx *ctx) const
+  {
     std::string str = cpp_dec_float_50::str(*this);
     return RedisModule_CreateString(ctx, str.data(), str.length());
   }
 
-  const RedisDecimal operator+(const RedisDecimal &rhs) const {
+  const RedisDecimal operator+(const RedisDecimal &rhs) const
+  {
     cpp_dec_float_50 result = static_cast<cpp_dec_float_50>(*this) + static_cast<cpp_dec_float_50>(rhs);
     return *static_cast<RedisDecimal<Prec> *>(&result);
   }
 
-  const RedisDecimal operator-(const RedisDecimal &rhs) const {
+  const RedisDecimal operator-(const RedisDecimal &rhs) const
+  {
     cpp_dec_float_50 result = static_cast<cpp_dec_float_50>(*this) - static_cast<cpp_dec_float_50>(rhs);
     return *static_cast<RedisDecimal<Prec> *>(&result);
   }
 
-  const RedisDecimal operator*(const RedisDecimal &rhs) const {
+  const RedisDecimal operator*(const RedisDecimal &rhs) const
+  {
     cpp_dec_float_50 result = static_cast<cpp_dec_float_50>(*this) * static_cast<cpp_dec_float_50>(rhs);
     return *static_cast<RedisDecimal<Prec> *>(&result);
   }
 
-  const RedisDecimal operator/(const RedisDecimal &rhs) const {
+  const RedisDecimal operator/(const RedisDecimal &rhs) const
+  {
     cpp_dec_float_50 result = static_cast<cpp_dec_float_50>(*this) / static_cast<cpp_dec_float_50>(rhs);
     return *static_cast<RedisDecimal<Prec> *>(&result);
   }
 };
 
-#endif //DECIMAL_MODULE_REDIS_DECIMAL_H
+#endif // DECIMAL_MODULE_REDIS_DECIMAL_H
